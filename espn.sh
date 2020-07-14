a78b2bbadd4a850efeb5eba7b8c77219736e1c48
Dillon Koch <dillonk428@gmail.com>
2020-07-14 14:40:39 -0500

    scraping ncaab

--- espn.sh
+++ espn.sh
@@ -5,8 +5,11 @@ conda activate bet
 cd ESPN_Scrapers
 
 # update game results for each league
+echo "Updating NFL Game Results..."
 python espn_update_results.py --league=NFL
+echo "Updating NBA Game Results..."
 python espn_update_results.py --league=NBA
+echo "Updating NCAAF Game Results..."
 python espn_update_results.py --league=NCAAF
 #python espn_update_results.py --league=NCAAB
 
